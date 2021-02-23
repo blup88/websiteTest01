379c395f7d0c24a3f9612690be6dd8a2bc75eb72
blup88 <46864169+blup88@users.noreply.github.com>
2021-02-23 14:49:10 +0100

    321
    41

--- Build/websiteTest01.framework.js
+++ Build/websiteTest01.framework.js
@@ -1300,7 +1300,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2801888;
+STATICTOP = STATIC_BASE + 2803168;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3286,7 +3286,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2801888;
+var STATIC_BUMP = 2803168;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
